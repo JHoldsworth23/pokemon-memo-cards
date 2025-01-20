4f45dff67a2a76ef2c0429f7f7b4b1de22c08d94
JHoldsworth23 <jacobph101@icloud.com>
2025-01-20 19:43:57 +0000

    create a new component to make buttons for each pokemon type and update state value

---
+++ src/components/pokemon-types-buttons.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+// props - setState function, array of text
+export default function PokemonTypeButtons({ callback, types }) {
+    return (
+        <>
+            {types.map((pokemonType, idx) => 
+                <button key={idx} onClick={() => callback(pokemonType)}>
+                    {pokemonType ? pokemonType : 'All'}
+                </button>
+            )}
+        </>
+    );
+}
