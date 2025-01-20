526ba14fa7175e9e739d7d28f58f87df3e5579ec
JHoldsworth23 <jacobph101@icloud.com>
2025-01-20 19:12:28 +0000

    add buttons for each pokemon types

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './App.css'
 
 export default function App() {
+  const [type, setType] = useState('');
+  
+  const pokemonTypes = [
+    '',
+    'fire',
+    'water',
+    'grass',
+    'electric',
+    'dragon',
+    'rock',
+    'ghost'
+  ];
+
   return (
     <>
       <h1>Pokémon Memo Cards</h1>
+      <p>{type}</p>
+      {pokemonTypes.map((pokemonType, idx) =>
+          <button key={idx} onClick={() => {setType(pokemonType ? pokemonType : "All")}}>
+            {pokemonType ? pokemonType : "All"}
+          </button>
+      )}
     </>
   )
 }
