d2dcbdc66badd3c8042d9c214a2bfb258c06d27b
JHoldsworth23 <jacobph101@icloud.com>
2025-01-21 18:54:42 +0000

    API Call for pokemon tcg data and use env variable for api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PokemonTypeButtons from './components/pokemon-types-buttons';
 import './App.css';
 
+const apiKey = import.meta.env.POKEMON_API_KEY;
 const POKEMONTYPES = ['', 'Fire', 'Water', 'Grass', 'Electric', 'Dragon', 'Rock', 'Ghost'];
 
 export default function App() {
   const [type, setType] = useState('');
 
+  useEffect(() => {
+    fetch('https://api.pokemontcg.io/v2/cards', {
+      headers: {
+        'X-Api-Key': apiKey,
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err.message));
+  })
   // use type variable to fetch a data from API request - useEffect
 
   return (
