c6c47b197ab8a715631ad2cd58c4b9522ec6b7a1
JHoldsworth23 <jacobph101@icloud.com>
2025-01-23 19:40:07 +0000

    add new state for difficulty and slice the array depending on difficulty

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from 'react';
 import PokemonCards from './components/pokemon-cards';
+import DifficultyButtons from './components/difficulty-buttons';
 import PokemonTypeButtons from './components/pokemon-types-buttons';
 import './App.css';
 
 const apiKey = import.meta.env.POKEMON_API_KEY;
 const POKEMONTYPES = ['', 'Fire', 'Water', 'Grass', 'Lightning', 'Dragon', 'Fighting', 'Darkness', 'Metal', 'Psychic'];
 
-function shuffle(arr) {
+function shuffleAndSlice(arr, difficulty) {
+  let cardNum = difficulty === 'easy' ? 10 : difficulty === 'normal' ? 18 : 25;
+
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
+    const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
+
+  return arr.slice(0, cardNum);
 }
 
 export default function App() {
   const [pokemon, setPokemon] = useState([]);
   const [type, setType] = useState('');
+  const [difficulty, setDifficulty] = useState('easy');
 
   useEffect(() => {
     const query = type ? `?q=types:${type}` : '';
@@ -28,21 +34,20 @@ export default function App() {
       .then(data => {
         console.log("Fetching a data...");
         const pokemonData = data.data;
-        shuffle(pokemonData)
-        // shuffle pokemon array and slice
-        // setPokemon(pokemonData);
-        // temporary display for 10 cards
-        setPokemon(pokemonData.slice(0, 10));
+        const pokemonArr = shuffleAndSlice(pokemonData, difficulty);
+        console.log(pokemonArr, pokemonArr.length);
+        setPokemon(pokemonArr);
       })
       .catch(err => console.log(err.message));
-  }, [type]);
-  // array dependency - type
+  }, [type, difficulty]);
+  // array dependency - type and difficulty
   // use type variable to fetch a data from API request - useEffect
 
   return (
     <>
       <h1>Pokémon Memo Cards</h1>
       <PokemonCards array={pokemon} />
+      <DifficultyButtons callback={setDifficulty} />
       <p>{type}</p>
       <PokemonTypeButtons callback={setType} types={POKEMONTYPES} />
     </>
