3d6d2ab72ac36c788086094fb141411c28e68565
JHoldsworth23 <jacobph101@icloud.com>
2025-01-26 18:44:03 +0000

    import shuffle-and-slice function and change onClick arrow function to shuffle the array

--- src/components/pokemon-cards.jsx
+++ src/components/pokemon-cards.jsx
@@ -1,7 +1,15 @@
-export default function PokemonCards({ cards, callback }) {
+import shuffleAndSlice from "../script/shuffle-and-slice";
+
+export default function PokemonCards({ cards, difficulty, check, setState }) {
     return (
         cards.map(card => 
-            <div key={card.id} onClick={() => callback(card.id)}>
+            <div 
+              key={card.id} 
+              onClick={() => {
+                check(card.id);
+                setState(shuffleAndSlice(cards, difficulty))
+              }}
+            >
                 <img src={card.images.small} alt={card.name} />
             </div>
         )
