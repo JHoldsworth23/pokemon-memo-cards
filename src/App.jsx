d1632314804e8354c3d3f8f018d6d67eb5185e8a
JHoldsworth23 <jacobph101@icloud.com>
2025-01-29 19:29:46 +0000

    import all icons and create an array of object for each pokemon type and icon

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,31 @@ import DifficultyButtons from './components/difficulty-buttons';
 import PokemonTypeButtons from './components/pokemon-types-buttons';
 import './App.css';
 
+// POKEMON ICONS
+import allTypes from "./assets/all-type.png";
+import fireType from "./assets/fire-type.png";
+import waterType from "./assets/water-type.png";
+import grassType from "./assets/grass-type.png";
+import lightningType from "./assets/lightning-type.png";
+import dragonType from "./assets/dragon-type.png";
+import fightingType from "./assets/fighting-type.png";
+import darknessType from "./assets/darkness-type.png";
+import metalType from "./assets/metal-type.png";
+import psychicType from "./assets/psychic-type.png";
+
 const apiKey = import.meta.env.POKEMON_API_KEY;
-const POKEMONTYPES = ['', 'Fire', 'Water', 'Grass', 'Lightning', 'Dragon', 'Fighting', 'Darkness', 'Metal', 'Psychic'];
+const POKEMONTYPES = [
+  { type: '', icon: allTypes }, 
+  { type: 'Fire', icon: fireType }, 
+  { type: 'Water', icon: waterType }, 
+  { type: 'Grass', icon: grassType }, 
+  { type: 'Lightning', icon: lightningType }, 
+  { type: 'Dragon', icon: dragonType }, 
+  { type: 'Fighting', icon: fightingType }, 
+  { type: 'Darkness', icon: darknessType }, 
+  { type: 'Metal', icon: metalType }, 
+  { type: 'Psychic', icon: psychicType },
+];
 
 export default function App() {
   const [pokemon, setPokemon] = useState([]);
