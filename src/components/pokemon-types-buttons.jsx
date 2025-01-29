6e51a087524c03db069608f75d3de6d491909e37
JHoldsworth23 <jacobph101@icloud.com>
2025-01-29 19:31:15 +0000

    add img element for pokemon type icon

--- src/components/pokemon-types-buttons.jsx
+++ src/components/pokemon-types-buttons.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-// props - setState function, array of text
 export default function PokemonTypeButtons({ onClick, types }) {
     return (
         <>
             {types.map((pokemonType, idx) => 
-                <button key={idx} onClick={() => onClick(pokemonType)}>
-                    {pokemonType ? pokemonType : 'All Types'}
+                <button key={idx} onClick={() => onClick(pokemonType.type)}>
+                    <img src={pokemonType.icon} alt="" />
+                    {pokemonType.type ? pokemonType.type : 'All Types'}
                 </button>
             )}
         </>
