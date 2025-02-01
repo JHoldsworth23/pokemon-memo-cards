52d0a015e4284b277f471f16510da71a81bda2e3
JHoldsworth23 <jacobph101@icloud.com>
2025-02-01 19:35:17 +0000

    add setState for each button to start the game

--- src/components/difficulty-buttons.jsx
+++ src/components/difficulty-buttons.jsx
@@ -1,22 +1,25 @@
 /* eslint-disable react/prop-types */
-export default function DifficultyButtons({ cards, onClick, setPokemon }) {
+export default function DifficultyButtons({ cards, onClick, setPokemon, setGameMode }) {
     return (
         <div>
             <button onClick={() => {
                 onClick('easy');
                 setPokemon(cards.slice(0, 10));
+                setGameMode("start");
             }}>
                 Easy
             </button>
             <button onClick={() => {
                 onClick('normal');
                 setPokemon(cards.slice(0, 18));
+                setGameMode("start");
             }}>
                 Normal
             </button>
             <button onClick={() => {
                 onClick('hard');
                 setPokemon(cards);
+                setGameMode("start");
             }}>
                 Hard
             </button>
