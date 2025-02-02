852d4fddde9d3881cac815adff7e06e060d8cc7c
JHoldsworth23 <jacobph101@icloud.com>
2025-02-02 20:32:34 +0000

    add the state to dynamically update the class to flip the card animation

--- src/components/pokemon-cards.jsx
+++ src/components/pokemon-cards.jsx
@@ -1,14 +1,21 @@
-export default function PokemonCards({ cards, onClick }) {
+import '../style/pokemon-cards.css';
+import BackCard from '../assets/pokemon-back-card.png';
+
+export default function PokemonCards({ cards, onClick, isFlipped }) {
   return (
     cards.map(card => 
-        <div 
-          key={card.id} 
-          onClick={() => {
-            onClick(card.id, cards);
-          }}
-        >
+      <div 
+        key={card.id}
+        onClick={() => onClick(card.id, cards)}
+        className={`pokemon-card ${isFlipped ? 'flipped' : ''}`}
+      >
+          <div className="front-card">
             <img src={card.images.small} alt={card.name} />
-        </div>
+          </div>
+          <div className="back-card" disabled={isFlipped}>
+            <img src={BackCard} alt="" />
+          </div>
+      </div>
     )
   );
 }
