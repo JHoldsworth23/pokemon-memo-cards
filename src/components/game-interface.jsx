af9a366534e8deff34b167d267a19c79773270c0
JHoldsworth23 <jacobph101@icloud.com>
2025-02-04 20:06:07 +0000

    create a new component for game interface

---
+++ src/components/game-interface.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import PokemonCards from "./pokemon-cards";
+
+export default function GameInterface({ 
+    pokemon, 
+    selectedPokemon,
+    gameMode,
+    difficulty,
+    checkPokemonCard,
+    isFlipped
+}) {
+    return (
+        <>
+          <p>Score: {`${selectedPokemon.length} / ${pokemon.length}`}</p> 
+          <div className={'all-pokemon-cards ' + difficulty}>
+            <PokemonCards 
+              cards={pokemon} 
+              difficulty={difficulty}
+              onClick={checkPokemonCard}
+              isFlipped={isFlipped}
+            />
+          </div>
+          {((gameMode.gameOver || gameMode.win)) && (
+            // this is a dialog modal to navigate the setting
+            <div>
+              <p>GAME OVER</p>
+              <div>
+                TWO BUTTONS TO RESET THE GAME OR SELECT NEW DIFFICULTY OR TYPE
+              </div>
+            </div>
+          )}
+        </>
+    )
+}
