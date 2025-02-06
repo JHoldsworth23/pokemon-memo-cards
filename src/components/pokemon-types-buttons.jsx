9a35f102bd58d68ca5c45a19627dadd06deedb4b
JHoldsworth23 <jacobph101@icloud.com>
2025-02-06 20:13:22 +0000

    add styles and class names for pokemon type buttons

--- src/components/pokemon-types-buttons.jsx
+++ src/components/pokemon-types-buttons.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable react/prop-types */
+import "../style/types-buttons.css";
+
 export default function PokemonTypeButtons({ onClick, types }) {
     return (
-        <>
-            {types.map((pokemonType, idx) => 
-                <button key={idx} onClick={() => onClick(pokemonType.type)}>
-                    <img src={pokemonType.icon} alt="" />
-                    {pokemonType.type ? pokemonType.type : 'All Types'}
-                </button>
-            )}
-        </>
+        <div className="pokemon-types">
+            <p>Select Any Pokémon Type</p>
+            <div className="all-type-buttons">
+                {types.map((pokemonType, idx) => 
+                    <button 
+                      key={idx} 
+                      onClick={() => onClick(pokemonType.type)}
+                      className="type-button"
+                    >
+                        <img src={pokemonType.icon} alt="" />
+                        {pokemonType.type ? pokemonType.type : 'All Types'}
+                    </button>
+                )}
+            </div>
+        </div>
     );
 }
