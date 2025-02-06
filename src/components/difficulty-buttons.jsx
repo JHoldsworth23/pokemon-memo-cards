0d079c757360aca7599788dd4f35a883cb08ea83
JHoldsworth23 <jacobph101@icloud.com>
2025-02-06 20:23:37 +0000

    add styles and class names for difficulty buttons

--- src/components/difficulty-buttons.jsx
+++ src/components/difficulty-buttons.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable react/prop-types */
+import "../style/difficulty-buttons.css";
+
 export default function DifficultyButtons({ cards, onClick }) {
     return (
-        <div>
-            <button onClick={() => onClick('easy', cards.slice(0, 10), "start")}>
-                Easy
-            </button>
-            <button onClick={() => onClick('normal', cards.slice(0, 18), "start")}>
-                Normal
-            </button>
-            <button onClick={() => onClick('hard', cards, "start")}>
-                Hard
-            </button>
+        <div className="difficulty-container">
+            <p>Select a difficulty level</p>
+            <div className="difficulty-buttons">
+                <button onClick={() => onClick('easy', cards.slice(0, 10), "start")}>
+                    Easy
+                </button>
+                <button onClick={() => onClick('normal', cards.slice(0, 18), "start")}>
+                    Normal
+                </button>
+                <button onClick={() => onClick('hard', cards, "start")}>
+                    Hard
+                </button>
+            </div>
         </div>
     );
 }
