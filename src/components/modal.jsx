1d11f1b7ce0fddd73c07728951f6ea177d288eac
JHoldsworth23 <jacobph101@icloud.com>
2025-02-08 22:23:32 +0000

    add and style overlay modal container when the game is ended

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
+import "../style/modal.css";
+
 export default function Modal({ resetGame, newGame }) {
     return (
         // this is a dialog modal to navigate the setting
-        <div>
-            {/* image of pokemon ball */}
-            <p>GAME OVER</p>
-            <div>
-                <button onClick={() => resetGame()}>RESET</button>
-                <button onClick={() => newGame()}>NEW GAME</button>
+        <div className="modal-container">
+            <div className="modal">
+                {/* image of pokemon ball */}
+                <p>GAME OVER</p>
+                <div>
+                    <button onClick={() => resetGame()}>RESET</button>
+                    <button onClick={() => newGame()}>NEW GAME</button>
+                </div>
             </div>
         </div>
     )
