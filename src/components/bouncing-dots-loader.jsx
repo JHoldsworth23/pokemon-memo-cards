92e51ae9175fff2027dd525789ea3ec207ce1792
JHoldsworth23 <jacobph101@icloud.com>
2025-02-08 21:23:23 +0000

    add new component for bouncing dots loading

---
+++ src/components/bouncing-dots-loader.jsx
@@ -0,0 +1,10 @@
+export default function BouncingDotsLoader() {
+    return (
+        <div className="loading-dots">
+            Loading
+            <span className="dot"></span>
+            <span className="dot"></span>
+            <span className="dot"></span>
+        </div>
+    )
+}
