b30de8a0b6b39522f63d6fa32a40fc1960c1a00d
JHoldsworth23 <jacobph101@icloud.com>
2025-02-08 21:23:39 +0000

    add bouncing dots component

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,10 +1,13 @@
 import loadingImage from '../assets/loading.png';
+import BouncingDotsLoader from './bouncing-dots-loader';
 import "../style/loading.css";
+
 
 export default function Loading() {
     return (
         <div className="loading">
             <img src={loadingImage} alt="" />
+            <BouncingDotsLoader />
         </div>
     )
 }
